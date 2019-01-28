39a59cacdb81fee6969b23ee7f482f810d361126
Youtao Xing <youtao.xing@icloud.com>
2019-01-28 08:57:01 +0800

    move context and math package to goos

--- ethclient/ethclient.go
+++ ethclient/ethclient.go
@@ -1,38 +1,37 @@
 package ethclient
 
 import (
-  "github.com/bcl-chain/web3.go/common"
-  "github.com/bcl-chain/web3.go/common/context"
-  "github.com/bcl-chain/web3.go/common/math/big"
-  "github.com/bcl-chain/web3.go/wrapper"
+	"github.com/ethereum/go-ethereum/ethclient"
 
-  "github.com/ethereum/go-ethereum/ethclient"
+	"github.com/bcl-chain/web3.go/common"
+	"github.com/bcl-chain/web3.go/goos/context"
+	"github.com/bcl-chain/web3.go/goos/math/big"
+	"github.com/bcl-chain/web3.go/wrapper"
 )
 
 type Client struct {
-  c interface{}
+	c interface{}
 }
 
 func Dial(rawurl string) (*Client, error) {
-  if c, err := ethclient.Dial(rawurl); err == nil {
-    return &Client{
-      c: c,
-    }, nil
-  } else {
-    return nil, err
-  }
+	if c, err := ethclient.Dial(rawurl); err == nil {
+		return &Client{
+			c: c,
+		}, nil
+	} else {
+		return nil, err
+	}
 }
 
 func (ec *Client) BalanceAt(wctx *context.Context, waccount *common.Address, wblockNumber *big.Int) (*big.Int, error) {
-  c, _ := ec.c.(*ethclient.Client)
-  context := wrapper.ToContext(wctx)
-  address := wrapper.ToAddress(waccount)
-  blockNumber := wrapper.ToBigInt(wblockNumber)
+	c, _ := ec.c.(*ethclient.Client)
+	context := wrapper.ToContext(wctx)
+	address := wrapper.ToAddress(waccount)
+	blockNumber := wrapper.ToBigInt(wblockNumber)
 
-  if bigInt, err :=  c.BalanceAt(context, address, blockNumber); err == nil {
-    return wrapper.FromBigInt(bigInt), nil
-  } else {
-    return nil, err
-  }
+	if bigInt, err := c.BalanceAt(context, address, blockNumber); err == nil {
+		return wrapper.FromBigInt(bigInt), nil
+	} else {
+		return nil, err
+	}
 }
-
--- examples/account_balance.go
+++ examples/account_balance.go
@@ -1,34 +1,34 @@
 package main
 
 import (
-//  "context"
-  "fmt"
-  "log"
+	//  "context"
+	"fmt"
+	"log"
 
-  "github.com/bcl-chain/web3.go/common"
-  "github.com/bcl-chain/web3.go/common/math/big"
-  "github.com/bcl-chain/web3.go/common/context"
-  "github.com/bcl-chain/web3.go/ethclient"
+	"github.com/bcl-chain/web3.go/common"
+	"github.com/bcl-chain/web3.go/ethclient"
+	"github.com/bcl-chain/web3.go/goos/context"
+	"github.com/bcl-chain/web3.go/goos/math/big"
 )
 
 func main() {
-  client, err := ethclient.Dial("http://127.0.0.1:8545")
-  if err != nil {
-    log.Fatal(err)
-  }
+	client, err := ethclient.Dial("http://127.0.0.1:8545")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  account := common.HexToAddress("0x0abb28e0270074d5552a66d5dd172fbcb9db4fd7")
-  balance, err := client.BalanceAt(context.Background(), account, nil)
-  if err != nil {
-    log.Fatal(err)
-  }
+	account := common.HexToAddress("0xfcbcc6047e8f7b12ba5f422666e4d8d7290a3459")
+	balance, err := client.BalanceAt(context.Background(), account, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  fmt.Println(balance)
+	fmt.Println(balance)
 
-  blockNumber := big.NewInt(3)
-  balanceAt, err := client.BalanceAt(context.Background(), account, blockNumber)
-  if err != nil {
-    log.Fatal(err)
-  }
-  fmt.Println(balanceAt)
+	blockNumber := big.NewInt(0)
+	balanceAt, err := client.BalanceAt(context.Background(), account, blockNumber)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(balanceAt)
 }
