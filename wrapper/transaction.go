60a3b41502ac55ad4692a2632097b76d29bfab34
Keisuke Katsushima <k-katsushima@nri.co.jp>
2019-01-28 15:25:49 +0800

    check indent

--- wrapper/transaction.go
+++ wrapper/transaction.go
@@ -3,28 +3,28 @@ package wrapper
 import (
   "github.com/ethereum/go-ethereum/core/types"
 
-  wtypes "github.com/bcl-chain/web3.go/core/types"
+	wtypes "github.com/bcl-chain/web3.go/core/types"
 )
 
 func FromTransactions(transactions *types.Transactions) *wtypes.Transactions {
-	if transactions != nil {
-		return &wtypes.Transactions{
-			Transactions: transactions,
-		}
-	} else {
-		return nil
-	}
+  if transactions != nil {
+    return &wtypes.Transactions{
+      Transactions: transactions,
+    }
+  } else {
+    return nil
+  }
 }
 
 func ToTransactions(wTransactions *wtypes.Transactions) *types.Transactions {
-	if wTransactions != nil {
-		if transactions, ok := wTransactions.Transactions.(*types.Transactions); ok {
-			return transactions
-		} else {
-			return nil
-		}
-	} else {
-		return nil
-	}
+  if wTransactions != nil {
+    if transactions, ok := wTransactions.Transactions.(*types.Transactions); ok {
+      return transactions
+    } else {
+      return nil
+    }
+  } else {
+    return nil
+  }
 }
 
