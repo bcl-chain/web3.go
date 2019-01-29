2a2ab8342e59c5a5f0ab6f1dd6e436afc7e2c031
Keisuke Katsushima <k-katsushima@nri.co.jp>
2019-01-29 15:30:15 +0800

    modified indent

--- ethclient/ethclient.go
+++ ethclient/ethclient.go
@@ -1,13 +1,13 @@
 package ethclient
 
 import (
-  "github.com/ethereum/go-ethereum/ethclient"
+	"github.com/ethereum/go-ethereum/ethclient"
 
 	"github.com/bcl-chain/web3.go/common"
 	"github.com/bcl-chain/web3.go/goos/context"
 	"github.com/bcl-chain/web3.go/goos/math/big"
 	"github.com/bcl-chain/web3.go/wrapper"
-  "github.com/bcl-chain/web3.go/core/types"
+	"github.com/bcl-chain/web3.go/core/types"
 )
 
 type Client struct {
@@ -38,35 +38,35 @@ func (ec *Client) BalanceAt(wctx *context.Context, waccount *common.Address, wbl
 }
 
 func (ec *Client) BlockByNumber(wctx *context.Context, wnumber *big.Int) (*types.Block, error) {
-  c, _ := ec.c.(*ethclient.Client)
-  context := wrapper.ToContext(wctx)
-  blockNumber := wrapper.ToBigInt(wnumber)
+	c, _ := ec.c.(*ethclient.Client)
+	context := wrapper.ToContext(wctx)
+	blockNumber := wrapper.ToBigInt(wnumber)
 
-  if block, err := c.BlockByNumber(context, blockNumber); err == nil {
-    return wrapper.FromBlock(block), nil
-  } else {
-    return nil, err
-  }
+	if block, err := c.BlockByNumber(context, blockNumber); err == nil {
+		return wrapper.FromBlock(block), nil
+	} else {
+		return nil, err
+	}
 }
 
 func (ec *Client) HeaderByNumber(wctx *context.Context, wnumber *big.Int) (*types.Header, error) {
-  c, _ := ec.c.(*ethclient.Client)
-  context := wrapper.ToContext(wctx)
-  blockNumber := wrapper.ToBigInt(wnumber)
+	c, _ := ec.c.(*ethclient.Client)
+	context := wrapper.ToContext(wctx)
+	blockNumber := wrapper.ToBigInt(wnumber)
 
-  if header, err := c.HeaderByNumber(context, blockNumber); err == nil {
-    return wrapper.FromHeader(header), nil
-  } else {
-    return nil, err
-  }
+	if header, err := c.HeaderByNumber(context, blockNumber); err == nil {
+		return wrapper.FromHeader(header), nil
+	} else {
+		return nil, err
+	}
 }
 
 func (ec *Client) TransactionByHash(wctx *context.Context, wblockHash *common.Hash) (*types.Transaction, bool, error) {
-  c, _ := ec.c.(*ethclient.Client)
-  context := wrapper.ToContext(wctx)
-  blockHash := wrapper.ToHash(wblockHash)
+	c, _ := ec.c.(*ethclient.Client)
+	context := wrapper.ToContext(wctx)
+	blockHash := wrapper.ToHash(wblockHash)
 
-  if tx, isPending, err := c.TransactionByHash(context, blockHash); err == nil {
+	if tx, isPending, err := c.TransactionByHash(context, blockHash); err == nil {
 		return wrapper.FromTransaction(tx), isPending, err
 	} else {
 		return nil, false, err
@@ -74,21 +74,21 @@ func (ec *Client) TransactionByHash(wctx *context.Context, wblockHash *common.Ha
 }
 
 func (ec *Client) TransactionCount(wctx *context.Context, wblockHash *common.Hash) (int64, error) {
-  c, _ := ec.c.(*ethclient.Client)
-  context := wrapper.ToContext(wctx)
-  blockHash := wrapper.ToHash(wblockHash)
+	c, _ := ec.c.(*ethclient.Client)
+	context := wrapper.ToContext(wctx)
+	blockHash := wrapper.ToHash(wblockHash)
 
-  count, err := c.TransactionCount(context, blockHash)
-  return int64(count), err
+	count, err := c.TransactionCount(context, blockHash)
+	return int64(count), err
 }
 
 func (ec *Client) TransactionInBlock(wctx *context.Context, wblockHash *common.Hash, index int64) (*types.Transaction, error) {
-  c, _ := ec.c.(*ethclient.Client)
-  context := wrapper.ToContext(wctx)
-  blockHash := wrapper.ToHash(wblockHash)
+	c, _ := ec.c.(*ethclient.Client)
+	context := wrapper.ToContext(wctx)
+	blockHash := wrapper.ToHash(wblockHash)
 	uindex := uint(index)
 
-  if tx, err := c.TransactionInBlock(context, blockHash, uindex); err == nil {
+	if tx, err := c.TransactionInBlock(context, blockHash, uindex); err == nil {
 		return wrapper.FromTransaction(tx), err
 	} else {
 		return nil, err
@@ -96,11 +96,11 @@ func (ec *Client) TransactionInBlock(wctx *context.Context, wblockHash *common.H
 }
 
 func (ec *Client) TransactionReceipt(wctx *context.Context, wblockHash *common.Hash) (*types.Receipt, error) {
-  c, _ := ec.c.(*ethclient.Client)
-  context := wrapper.ToContext(wctx)
-  blockHash := wrapper.ToHash(wblockHash)
+	c, _ := ec.c.(*ethclient.Client)
+	context := wrapper.ToContext(wctx)
+	blockHash := wrapper.ToHash(wblockHash)
 
-  if receipt, err := c.TransactionReceipt(context, blockHash); err == nil {
+	if receipt, err := c.TransactionReceipt(context, blockHash); err == nil {
 		return wrapper.FromReceipt(receipt), nil
 	} else {
 		return nil, err
