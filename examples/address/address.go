41e21d902d73a1a93c3e6de4f5b8416622ae8b91
astra <youtao.xing@icloud.com>
2019-05-22 18:26:36 +0800

    add example that compute address from privateKey

--- examples/address/address.go
+++ examples/address/address.go
@@ -16,4 +16,9 @@ func main() {
 	fmt.Println(address.GetHex())
 	fmt.Println(address.GetHash().GetHex())
 	fmt.Println(address.GetBytes())
+
+	privKey, _ := web3go.HexToECDSA("943c44a3f2bbc4230ef40556cd9a6ad46f2e4ce5f2e04f17573a168b8356f3f0")
+	pubKey := privKey.Public()
+	address = web3go.PubkeyToAddress(pubKey)
+	fmt.Println(address.GetHex())
 }
